05df3475daf7392117aa8b2c765bf2e0ba5f1362
Michael Girard <michael.girard@gstv.com>
2015-06-23 14:21:13 -0400

    Added message acknowledgements

--- worker.py
+++ worker.py
@@ -15,8 +15,9 @@ def callback(ch, method, properties, body):
     print " [x] Received %r" % (body,)
     time.sleep( body.count('.') )
     print " [x] Done"
+    ch.basic_ack(delivery_tag = method.delivery_tag)
 
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume(callback, queue='hello')
 
 channel.start_consuming()
 
